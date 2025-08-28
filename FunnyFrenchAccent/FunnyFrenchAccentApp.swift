76a9370a08d28e6898b7b0dd73382d9a45f502fa
Sinan Karasu <sinan@bozuk.org>
2025-08-27 17:36:29 -0700

    Initial Commit

---
+++ FunnyFrenchAccent/FunnyFrenchAccentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunnyFrenchAccentApp.swift
+//  FunnyFrenchAccent
+//
+//  Created by Sinan Karasu on 8/27/25.
+//
+
+import SwiftUI
+
+@main
+struct FunnyFrenchAccentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
